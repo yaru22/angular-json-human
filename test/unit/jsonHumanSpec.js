d994bc6b391d8025f6e4f31858435958f92c6dc5
Brian Park <yaru22@gmail.com>
2013-09-18 22:29:48 -0400

    Added an empty unit test.

---
+++ test/unit/jsonHumanSpec.js
@@ -0,0 +1,28 @@
+/* globals angular, beforeEach, chai, describe, inject, it */
+'use strict';
+
+var expect = chai.expect;
+
+describe('hovercard', function() {
+  var elm, scope;
+
+  // load the code
+  beforeEach(module('yaru22.directives.jsonHuman'));
+
+  // load the templates
+  beforeEach(module('tmpl'));
+
+  beforeEach(inject(function($rootScope, $compile) {
+    // we might move this tpl into an html file as well...
+    elm = angular.element();
+
+    scope = $rootScope;
+    $compile(elm)(scope);
+    scope.$digest();
+  }));
+
+
+  it('should pass', inject(function() {
+    expect(1).to.equal(1);
+  }));
+});
